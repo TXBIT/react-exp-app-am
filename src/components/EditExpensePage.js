66df4f5a0b6c2fe4b71fb665dc6b96be5d3f2c02
TXBIT <shenlong203@gmail.com>
2020-08-14 21:10:23 +0700

    020 Wiring up Edit Expense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-  return <div>Editing the expense with id of {props.match.params.id}</div>;
+const EditExpensePage = ({ expense, history, editExpense, removeExpense }) => {
+  return (
+    <div>
+      <ExpenseForm
+        expense={expense}
+        onSubmit={(updatedExpense) => {
+          editExpense(expense.id, updatedExpense);
+          history.push('/');
+        }}
+      />
+      <button
+        onClick={() => {
+          removeExpense({ id: expense.id });
+          history.push('/');
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => ({
+  expense: state.expenses.find(
+    (expense) => expense.id === props.match.params.id
+  ),
+});
+
+export default connect(mapStateToProps, { editExpense, removeExpense })(
+  EditExpensePage
+);
