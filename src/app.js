6ac672e979561f89a4eaae4e1a0dcb36fb864adc
TXBIT <shenlong203@gmail.com>
2020-08-15 16:57:33 +0700

    031 Mocking Libraries with Jest

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
 import { Provider } from 'react-redux';
+import 'normalize.css/normalize.css';
+import 'react-dates/lib/css/_datepicker.css';
 
 import './styles/styles.scss';
 import configureStore from './store/configureStore';
