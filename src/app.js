6957b7c77060e0c27b60106a9e1bc4f48adf965b
TXBIT <shenlong203@gmail.com>
2020-08-16 21:59:42 +0700

    045 Fetching Expenses

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import './styles/styles.scss';
 import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
 import './firebase/firebase';
+import { startSetExpenses } from './actions/expenses';
 
 const store = configureStore();
 
@@ -17,4 +18,8 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
